b328a0c262f66543bc762e7e3698d0262adc00bd
Vladislav <burdukovskij.vlad@mail.ru>
2021-08-04 18:06:32 +0300

    10% анимации

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,33 +1,47 @@
 <template>
   <div class="homePage-wrapper">
     <div class="container-fluid p-0 homePage">
-      <div
-        class="homePage-title"
-        data-aos="fade-down"
-        data-aos-offset="300"
-        data-aos-easing="ease-in-sine"
-        data-aos-duration="1500"
-      >
-        <h3>СОРЕМЕННОЕ IT АГЕНСТВО</h3>
-        <p>We have developed:</p>
-      </div>
+      <!-- <div class="homePage-title homePage-title-main">
+        <div
+          class=""
+          data-aos="fade-down"
+          data-aos-offset="300"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1500"
+        >
+          <h3>СОРЕМЕННОЕ IT АГЕНСТВО</h3>
+          <p class="text-center">We have developed:</p>
+        </div>
+      </div> -->
       <slideHome />
-      <div class="slideHome-progres">
+
+      <!-- <div class="slideHome-progres">
         <div class="slideHome-progres-bar"></div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
+import "@/assets/scriptsJS/homePage.js";
 import slideHome from "@/pages/home/components/Slider";
 export default {
   // transition() {
   //   return "slide";
   // },
   name: "Home",
+  data() {
+    return {
+      active: false,
+    };
+  },
   components: {
     slideHome,
+  },
+  methods: {
+    // showGhildAnim() {
+    //   console.log(11);
+    // },
   },
 };
 </script>
